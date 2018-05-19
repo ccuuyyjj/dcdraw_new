4dcaaa8d72a1b3cb43370b4bac3d1b3eceb20577
Core <Core@CORE-PC-X64>
2018-05-20 00:25:30 +0900

    로깅 부분 수정

---
+++ dcdraw_new/src/dcdraw/servlet/LogDisplayServlet.java
@@ -0,0 +1,300 @@
+package dcdraw.servlet;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.security.AccessController;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.servlet.ServletException;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.catalina.Globals;
+import org.apache.catalina.WebResource;
+import org.apache.catalina.servlets.DefaultServlet;
+import org.apache.catalina.util.ServerInfo;
+import org.apache.tomcat.util.security.Escape;
+import org.apache.tomcat.util.security.PrivilegedGetTccl;
+import org.apache.tomcat.util.security.PrivilegedSetTccl;
+
+public class LogDisplayServlet extends DefaultServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4189376761567833222L;
+	protected DateFormat dateformat = new SimpleDateFormat("E, d MMM yyyy HH:mm:ss");
+	{
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+	
+	@Override
+	protected InputStream renderHtml(String contextPath, WebResource resource, String encoding) throws IOException {
+
+        // Prepare a writer to a buffered area
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        OutputStreamWriter osWriter = new OutputStreamWriter(stream, "UTF8");
+        PrintWriter writer = new PrintWriter(osWriter);
+
+        StringBuilder sb = new StringBuilder();
+
+        String[] entries = resources.list(resource.getWebappPath());
+
+        // rewriteUrl(contextPath) is expensive. cache result for later reuse
+        String rewrittenContextPath =  rewriteUrl(contextPath);
+        String directoryWebappPath = resource.getWebappPath();
+
+        // Render the page header
+        sb.append("<html>\r\n");
+        sb.append("<head>\r\n");
+        sb.append("<title>");
+        sb.append(sm.getString("directory.title", directoryWebappPath));
+        sb.append("</title>\r\n");
+        sb.append("<STYLE><!--");
+        sb.append(org.apache.catalina.util.TomcatCSS.TOMCAT_CSS);
+        sb.append("--></STYLE> ");
+        sb.append("</head>\r\n");
+        sb.append("<body>");
+        sb.append("<h1>");
+        sb.append(sm.getString("directory.title", directoryWebappPath));
+
+        // Render the link to our parent (if required)
+        String parentDirectory = directoryWebappPath;
+        if (parentDirectory.endsWith("/")) {
+            parentDirectory =
+                parentDirectory.substring(0, parentDirectory.length() - 1);
+        }
+        int slash = parentDirectory.lastIndexOf('/');
+        if (slash >= 0) {
+            String parent = directoryWebappPath.substring(0, slash);
+            sb.append(" - <a href=\"");
+            sb.append(rewrittenContextPath);
+            if (parent.equals(""))
+                parent = "/";
+            sb.append(rewriteUrl(parent));
+            if (!parent.endsWith("/"))
+                sb.append("/");
+            sb.append("\">");
+            sb.append("<b>");
+            sb.append(sm.getString("directory.parent", parent));
+            sb.append("</b>");
+            sb.append("</a>");
+        }
+
+        sb.append("</h1>");
+        sb.append("<HR size=\"1\" noshade=\"noshade\">");
+
+        sb.append("<table width=\"100%\" cellspacing=\"0\"" +
+                     " cellpadding=\"5\" align=\"center\">\r\n");
+
+        // Render the column headings
+        sb.append("<tr>\r\n");
+        sb.append("<td align=\"left\"><font size=\"+1\"><strong>");
+        sb.append(sm.getString("directory.filename"));
+        sb.append("</strong></font></td>\r\n");
+        sb.append("<td align=\"center\"><font size=\"+1\"><strong>");
+        sb.append(sm.getString("directory.size"));
+        sb.append("</strong></font></td>\r\n");
+        sb.append("<td align=\"right\"><font size=\"+1\"><strong>");
+        sb.append(sm.getString("directory.lastModified"));
+        sb.append("</strong></font></td>\r\n");
+        sb.append("</tr>");
+
+        boolean shade = false;
+        for (String entry : entries) {
+            if (entry.equalsIgnoreCase("WEB-INF") ||
+                entry.equalsIgnoreCase("META-INF") ||
+                entry.equalsIgnoreCase("SAFE_TO_DELETE"))
+                continue;
+
+            WebResource childResource =
+                    resources.getResource(directoryWebappPath + entry);
+            if (!childResource.exists()) {
+                continue;
+            }
+
+            sb.append("<tr");
+            if (shade)
+                sb.append(" bgcolor=\"#eeeeee\"");
+            sb.append(">\r\n");
+            shade = !shade;
+
+            sb.append("<td align=\"left\">&nbsp;&nbsp;\r\n");
+            sb.append("<a href=\"");
+            sb.append(rewrittenContextPath);
+            sb.append(rewriteUrl(directoryWebappPath + entry));
+            if (childResource.isDirectory())
+                sb.append("/");
+            sb.append("\"><tt>");
+            sb.append(Escape.htmlElementContent(entry));
+            if (childResource.isDirectory())
+                sb.append("/");
+            sb.append("</tt></a></td>\r\n");
+
+            sb.append("<td align=\"right\"><tt>");
+            if (childResource.isDirectory())
+                sb.append("&nbsp;");
+            else
+                sb.append(renderSize(childResource.getContentLength()));
+            sb.append("</tt></td>\r\n");
+
+            sb.append("<td align=\"right\"><tt>");
+            sb.append(dateformat.format(new Date(childResource.getLastModified()))).append(" KST");
+            sb.append("</tt></td>\r\n");
+
+            sb.append("</tr>\r\n");
+        }
+
+        // Render the page footer
+        sb.append("</table>\r\n");
+
+        sb.append("<HR size=\"1\" noshade=\"noshade\">");
+
+        String readme = getReadme(resource, encoding);
+        if (readme!=null) {
+            sb.append(readme);
+            sb.append("<HR size=\"1\" noshade=\"noshade\">");
+        }
+
+        if (showServerInfo) {
+            sb.append("<h3>").append(ServerInfo.getServerInfo()).append("</h3>");
+        }
+        sb.append("</body>\r\n");
+        sb.append("</html>\r\n");
+
+        // Return an input stream to the underlying bytes
+        writer.write(sb.toString());
+        writer.flush();
+        return new ByteArrayInputStream(stream.toByteArray());
+
+    }
+	
+	@Override
+	protected InputStream renderXml(String contextPath, WebResource resource, Source xsltSource, String encoding)
+			throws IOException, ServletException {
+		
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<?xml version=\"1.0\"?>");
+        sb.append("<listing ");
+        sb.append(" contextPath='");
+        sb.append(contextPath);
+        sb.append("'");
+        sb.append(" directory='");
+        sb.append(resource.getName());
+        sb.append("' ");
+        sb.append(" hasParent='").append(!resource.getName().equals("/"));
+        sb.append("'>");
+
+        sb.append("<entries>");
+
+        String[] entries = resources.list(resource.getWebappPath());
+
+        // rewriteUrl(contextPath) is expensive. cache result for later reuse
+        String rewrittenContextPath =  rewriteUrl(contextPath);
+        String directoryWebappPath = resource.getWebappPath();
+
+        for (String entry : entries) {
+
+            if (entry.equalsIgnoreCase("WEB-INF") ||
+                    entry.equalsIgnoreCase("META-INF") ||
+                    entry.equalsIgnoreCase(localXsltFile))
+                continue;
+
+            if ((directoryWebappPath + entry).equals(contextXsltFile))
+                continue;
+
+            WebResource childResource =
+                    resources.getResource(directoryWebappPath + entry);
+            if (!childResource.exists()) {
+                continue;
+            }
+
+            sb.append("<entry");
+            sb.append(" type='")
+              .append(childResource.isDirectory()?"dir":"file")
+              .append("'");
+            sb.append(" urlPath='")
+              .append(rewrittenContextPath)
+              .append(rewriteUrl(directoryWebappPath + entry))
+              .append(childResource.isDirectory()?"/":"")
+              .append("'");
+            if (childResource.isFile()) {
+                sb.append(" size='")
+                  .append(renderSize(childResource.getContentLength()))
+                  .append("'");
+            }
+			sb.append(" date='")
+			  .append(dateformat.format(new Date(childResource.getLastModified()))).append(" KST")
+			  .append("'");
+
+            sb.append(">");
+            sb.append(Escape.htmlElementContent(entry));
+            if (childResource.isDirectory())
+                sb.append("/");
+            sb.append("</entry>");
+        }
+        sb.append("</entries>");
+
+        String readme = getReadme(resource, encoding);
+
+        if (readme!=null) {
+            sb.append("<readme><![CDATA[");
+            sb.append(readme);
+            sb.append("]]></readme>");
+        }
+
+        sb.append("</listing>");
+
+        // Prevent possible memory leak. Ensure Transformer and
+        // TransformerFactory are not loaded from the web application.
+        ClassLoader original;
+        if (Globals.IS_SECURITY_ENABLED) {
+            PrivilegedGetTccl pa = new PrivilegedGetTccl();
+            original = AccessController.doPrivileged(pa);
+        } else {
+            original = Thread.currentThread().getContextClassLoader();
+        }
+        try {
+            if (Globals.IS_SECURITY_ENABLED) {
+                PrivilegedSetTccl pa =
+                        new PrivilegedSetTccl(DefaultServlet.class.getClassLoader());
+                AccessController.doPrivileged(pa);
+            } else {
+                Thread.currentThread().setContextClassLoader(
+                        DefaultServlet.class.getClassLoader());
+            }
+
+            TransformerFactory tFactory = TransformerFactory.newInstance();
+            Source xmlSource = new StreamSource(new StringReader(sb.toString()));
+            Transformer transformer = tFactory.newTransformer(xsltSource);
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            OutputStreamWriter osWriter = new OutputStreamWriter(stream, "UTF8");
+            StreamResult out = new StreamResult(osWriter);
+            transformer.transform(xmlSource, out);
+            osWriter.flush();
+            return new ByteArrayInputStream(stream.toByteArray());
+        } catch (TransformerException e) {
+            throw new ServletException("XSL transformer error", e);
+        } finally {
+            if (Globals.IS_SECURITY_ENABLED) {
+                PrivilegedSetTccl pa = new PrivilegedSetTccl(original);
+                AccessController.doPrivileged(pa);
+            } else {
+                Thread.currentThread().setContextClassLoader(original);
+            }
+        }
+	}
+}
